7eb035afd81aa5f22d7f52af2e014fc47bf4dd32
Anuja Nimmagadda <anu@Anujas-MacBook-Pro.local>
2025-12-07 19:50:42 -0700

    Remove section navs (declutter)
    DELETED:
    - AboutMeSectionNav.tsx
    - Removed from PageShell.tsx
    
    Both landing page and Me page now have cleaner layouts
    with just the main navbar. Pages are short enough to scroll.

--- src/components/layout/PageShell.tsx
+++ src/components/layout/PageShell.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { ReactNode } from 'react'
-import { usePathname } from 'next/navigation'
 import SiteHeader from './SiteHeader'
 import SiteFooter from './SiteFooter'
 import SkipToContent from '@/components/accessibility/SkipToContent'
 import ReadingProgress from '@/components/case-study/ReadingProgress'
 import BackToTop from '@/components/navigation/BackToTop'
-import AboutMeSectionNav from '@/components/navigation/AboutMeSectionNav'
 import URLHashSync from '@/components/navigation/URLHashSync'
 import { ErrorBoundary } from '@/components/error/ErrorBoundary'
 
@@ -16,14 +14,11 @@ interface PageShellProps {
 }
 
 export default function PageShell({ children }: PageShellProps) {
-  const pathname = usePathname()
-
   return (
     <ErrorBoundary>
       <SkipToContent />
       <ReadingProgress />
       <SiteHeader />
-      {(pathname === '/me' || pathname === '/me/') && <AboutMeSectionNav />}
       <URLHashSync />
       <main id="main-content">
         {children}
